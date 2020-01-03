b7f354b113966b79149b35e82919956310ab323e
littleflute <littleflute@163.com>
2020-01-02 21:55:54 -0800

    xdtest1

--- examples/2.0/controllers/Weather.js
+++ examples/2.0/controllers/Weather.js
@@ -4,6 +4,7 @@ var weather = require('weather-js');
 
 module.exports.getWeather = function getWeather (req, res, next) {
   // Code necessary to consume the Weather API and respond
+  console.log("xdtest..." + req.swagger.params.location.value + ":" + req.swagger.params.unit.value);
   weather.find({
     search: req.swagger.params.location.value,
     degreeType: req.swagger.params.unit.value
